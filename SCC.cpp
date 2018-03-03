2c007c57184db345b31d4b3944da35febb9b5ab2
Hardik Shah <hardik.shah@discovery-robotics.com>
2018-03-03 18:05:10 +0530

    Optimized the code and removed random print statements

--- SCC.cpp
+++ SCC.cpp
@@ -21,6 +21,7 @@ public:
 	linked_list(){
 		head	=	NULL,tail 	=	NULL;
 	}
+
 	int leader;
 	void add_Node(Node* start);
 	void print_list();
@@ -60,6 +61,7 @@ void linked_list::print_list()
 	else 
 		cout<<"empty"<<endl;
 }
+
 void linked_list::delete_list()
 {
 	while(head!=NULL)
@@ -70,10 +72,11 @@ void linked_list::delete_list()
 	}
 }
 
+
 std::vector<linked_list> reverseGraph(std::vector<linked_list> v)
 {
 	std::vector<linked_list> rev_graph;
-	for(int i=0;i<v.size();i++)
+	for(auto i=0;i<v.size();i++)
 	{
 		linked_list l1;
 		Node *start = new Node();
@@ -82,7 +85,7 @@ std::vector<linked_list> reverseGraph(std::vector<linked_list> v)
 		rev_graph.push_back(l1);
 	}
 	
-	for(int i=0;i<v.size();i++)
+	for(auto i=0;i<v.size();i++)
 	{
 		if(v[i].head!=NULL)
 		{
@@ -101,7 +104,7 @@ std::vector<linked_list> reverseGraph(std::vector<linked_list> v)
 	return rev_graph;
 }
 
-void DFS_recursive(std::vector<linked_list> &network, int &current_label,int vertex)
+void DFS_recursive(std::vector<linked_list> &network, int &current_label,const int vertex)
 {
 	Node *pos = network[vertex].head;
 	if(network[vertex].head->explored==false)
@@ -114,27 +117,24 @@ void DFS_recursive(std::vector<linked_list> &network, int &current_label,int ver
 				DFS_recursive(network,current_label,(pos->num_Vertex)-1);
 		}
 
-		// cout<<"Current Label is: "<<current_label<<endl;
-		// cout<<"vertex is: "<<vertex<<endl;
 		network[vertex].head->leader_num = current_label;
 		current_label--;
-		//cout<<"The current label is: "<<current_label<<endl;
 	}
 
 }
 
-void DFS_main_Loop(std::vector<linked_list>& network)
+void DFS_main_Loop(std::vector<linked_list> &network)
 {
 	int current_label = network.size();
 
-	for(int i=network.size()-1;i>=0;i--)//<network.size();i++)
+	for(auto i=network.size()-1;i>=0;i--)//<network.size();i++)
 	{
 		if(network[i].head->explored==false)
 			DFS_recursive(network,current_label,i);
 	}
 }
 
-void DFS_second_recursive(std::vector<linked_list> &network, int &current_label, int vertex)
+void DFS_second_recursive(std::vector<linked_list> &network, int &current_label, const int vertex)
 {
 	Node *pos = network[vertex].head;
 	if(network[vertex].head->explored==false)
@@ -147,18 +147,13 @@ void DFS_second_recursive(std::vector<linked_list> &network, int &current_label,
 			if(network[pos->num_Vertex-1].head->explored==false)
 				DFS_second_recursive(network,current_label,pos->num_Vertex-1);
 		}
-		//network[pos->num_Vertex-1].head->leader_num=current_label;
-		// cout<<"Current Label is: "<<current_label<<endl;
-		// cout<<"vertex is: "<<vertex<<endl;
-		//
-		//current_label++;//--;current_label++;
 	}
 		
 }
 void DFS_second_main_Loop(std::vector<linked_list> &rev_network,std::vector<linked_list> &network)
 {
-	int current_label = 1;//network.size();
-	for(int i=0;i<rev_network.size();i++)//<network.size();i++)
+	auto current_label = 1;//network.size();
+	for(auto i=0;i<rev_network.size();i++)//<network.size();i++)
 	{
 		if(network[rev_network[i].head->num_Vertex-1].head->explored==false)
 		{
@@ -166,30 +161,19 @@ void DFS_second_main_Loop(std::vector<linked_list> &rev_network,std::vector<link
 			current_label++;
 		}
 	}
-	// for(int i=0;i<network.size();i++)
-	// 	std::cout<<"Vertex is: "<<network[i].head->num_Vertex<<" and the leader is: "<<network[i].head->leader_num<<endl;
 }
 
-bool sortLeader( linked_list l1,linked_list l2)
+bool sortLeader(const linked_list l1,const linked_list l2)
 {
 	return l1.head->leader_num < l2.head->leader_num;
 }
 
-// bool sortLeaderColumn(linked_list l1,linked_list l2)
-// {
-
-// }
 
 void SCC_final_loop(std::vector<linked_list> &rev_network, std::vector<linked_list> &network)
 {
-	// for(int i=0;i<rev_network.size();i++)
-	// 	std::cout<<"The vertex is: "<<rev_network[i].head->num_Vertex<<" and the finishing time is: "<<rev_network[i].head->leader_num<<endl;
 	std::sort(rev_network.begin(),rev_network.end(),sortLeader);
-	//std::cout<<"in sorted array , the first head has vertex"<<rev_network[0].head->num_Vertex<<" and the leader number is: "<<rev_network[0].head->leader_num<<endl;
 	DFS_second_main_Loop(rev_network,network);
-
 }
-
 
 
 int main ()
@@ -197,10 +181,10 @@ int main ()
 	vector <linked_list> Graph,Graph1;
 	int n;
 	string line;
-	//cout<<"Enter the number of nodes in the network"<<endl;
-	//cin>>n;
-	for (int i=1;i<=320000;i++)
-	// for (int i=1;i<=9;i++)
+	cout<<"Enter the number of nodes in the network"<<endl;
+	cin>>n;
+
+	for (auto i=1;i<=n;i++)
 	{
 		linked_list l1;
 		Node *start = new Node();
@@ -208,11 +192,18 @@ int main ()
 		l1.add_Node(start);
 		Graph.push_back(l1);
 	}
+
 	ifstream SCC_file;
-	SCC_file.open("./test_cases/input_mostlyCycles_68_320000.txt");
+	string filename;
+	cin>>filename;
+	string filepath	="./test_cases/";
+	string finalPath = filepath		+	filename;
+	bool fileFound=false;
+
+	SCC_file.open(finalPath);
 	if(SCC_file.is_open())
 	{
-		cout<<"Found the file"<<endl;
+		fileFound =true;
 		while(!SCC_file.eof())
 		{
 			getline(SCC_file,line);
@@ -231,59 +222,54 @@ int main ()
 			Graph[head_index-1].add_Node(edge);
 		}
 	}
-	
-
-	SCC_file.close();
-	cout<<"Finished generating the graph"<<endl;
-	// for (int i=0;i<Graph.size();i++)
-	// 	Graph[i].print_list();
-	//Graph[875713].print_list();
-	Graph1 = reverseGraph(Graph);
-	cout<<"Finished reversing the graph"<<endl;
-	DFS_main_Loop(Graph1);
-	// for(int i=0;i<Graph1.size();i++)
-	// {
-	// 	Graph1[i].print_list();
-	// 	cout<<"The current labels are: "<<Graph1[i].head->leader_num<<endl;
-	// }
-	cout<<"Finished DFS on the reverse graph"<<endl;
-	SCC_final_loop(Graph1,Graph);
-
-	sort(Graph.begin(),Graph.end(),sortLeader);
-	cout<<"Finished SCC on the  graph"<<endl;
-	// for(int i=0;i<Graph.size();i++)
-	// 	cout<<"The leader for vertex: "<<Graph[i].head->num_Vertex<<" is: "<<Graph[i].head->leader_num<<endl;
-
-	std::vector<int> LeaderCount;
-	for(int i=0;i<5;i++)
-		LeaderCount.push_back(0);
-
-	int count =0;
-	int oldLeader=Graph[0].head->leader_num;
-	bool reset =false;
-	for(int i=0;i<Graph.size();i++)
+	else 
 	{
-		int newLeader= Graph[i].head->leader_num;
-		if( newLeader == oldLeader)
-		{
-			count++;
-			reset = false;
-		}
-		else 
-		{
-			LeaderCount.push_back(count);
-			count =1;
-			oldLeader=newLeader;
-			reset =true;
-		}
+		cout<<"File not found please check file path or name"<<endl;
 	}
 
-	if(reset ==false)
-		LeaderCount.push_back(count);
+	SCC_file.close();
+	if(fileFound==true)
+	{
+		cout<<"Finished generating the graph"<<endl;
+		Graph1 = reverseGraph(Graph);
+		cout<<"Finished reversing the graph"<<endl;
+		DFS_main_Loop(Graph1);
+		cout<<"Finished DFS on the reverse graph"<<endl;
+		SCC_final_loop(Graph1,Graph);
+		sort(Graph.begin(),Graph.end(),sortLeader);
+		cout<<"Finished SCC on the  graph"<<endl;
 
-	sort(LeaderCount.begin(),LeaderCount.end());
-	for(int i=LeaderCount.size()-1;i>LeaderCount.size()-6;i--)
-		cout<<"Leader count is: "<<LeaderCount[i]<<endl;
+		std::vector<int> LeaderCount;
+		for(auto i=0;i<5;i++)
+			LeaderCount.push_back(0);
+
+		auto count =0;
+		auto oldLeader=Graph[0].head->leader_num;
+		bool reset =false;
+		for(auto i=0;i<Graph.size();i++)
+		{
+			auto newLeader= Graph[i].head->leader_num;
+			if( newLeader == oldLeader)
+			{
+				count++;
+				reset = false;
+			}
+			else 
+			{
+				LeaderCount.push_back(count);
+				count =1;
+				oldLeader=newLeader;
+				reset =true;
+			}
+		}
+
+		if(reset ==false)
+			LeaderCount.push_back(count);
+
+		sort(LeaderCount.begin(),LeaderCount.end());
+		for(auto i = LeaderCount.size()-1; i > LeaderCount.size()-6; i--)
+			cout<<"Leader count is: "<<LeaderCount[i]<<endl;
+	}
 
 	return 0;
 }
